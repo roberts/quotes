91251f0f6c44c10b4398ca5daa2a8bbb7a5c9319
DrewRoberts83 <drew@roberts.email>
2017-01-09 22:33:34 +0000

    Basic Files

---
+++ database/migrations/2017_01_09_215714_create_authors_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAuthorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('authors', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('slug');
+            $table->string('first_name');
+            $table->string('last_name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('authors');
+    }
+}
---
+++ database/migrations/2017_01_09_215758_create_quotes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quotes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->text('quote')
+            $table->integer('author_id')
+            $table->integer('created_by')
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('quotes');
+    }
+}
