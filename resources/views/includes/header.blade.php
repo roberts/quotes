0c6cdf993d39a3bf909d08515fe7c56138dec08e
DrewRoberts83 <drew@roberts.email>
2017-02-15 17:08:21 +0000

    Menu edit on header.blade

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -2,6 +2,6 @@
     <nav>
         <li><a href="{{ route('home') }}">Home</a></li>
         <li><a href="{{ route('quotes') }}">Quotes</a></li>
-        <li><a href="#">Sports</a></li>
+        <li><a href="#">Books</a></li>
     </nav>
 </header>
