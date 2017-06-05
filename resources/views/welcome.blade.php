5cea21f2881c92ec7fb6aca7f7ef15fc8ab35211
DrewRoberts <drew@roberts.email>
2017-06-05 16:23:58 -0400

    GA Tracking

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -87,6 +87,15 @@
     </style>
 </head>
 <body>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+      ga('create', 'UA-100525006-1', 'auto');
+      ga('send', 'pageview');
+    </script>
+
     <div class="full-height flex-column">
         <nav class="links">
             <a href="/quotes" style="margin-right: 15px;">
