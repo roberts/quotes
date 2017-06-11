ddbdb5c53db9ed49e232cd4ea9301af2f123a31a
DrewRoberts <drew@roberts.email>
2017-06-11 11:25:58 -0400

    Added Quotes

--- database/migrations/2017_04_03_194023_create_book_isbn_table.php
+++ database/migrations/2017_04_03_194023_create_book_isbn_table.php
@@ -15,6 +15,29 @@ class CreateBookIsbnTable extends Migration
     {
         Schema::create('book_isbn', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('title');
+            $table->string('author');
+            $table->unsignedInteger('author_id')->nullable();
+            $table->string('author_bio')->nullable();
+            $table->string('authors')->nullable();
+            $table->string('title_slug')->nullable();
+            $table->string('author_slug')->nullable();
+            $table->string('isbn13', 13)->nullable();
+            $table->string('isbn10', 10)->nullable();
+            $table->string('price')->nullable();
+            $table->string('format')->nullable();
+            $table->string('publisher')->nullable();
+            $table->string('pubdate')->nullable();
+            $table->string('edition')->nullable();
+            $table->string('subjects')->nullable();
+            $table->string('lexile')->nullable();
+            $table->string('pages')->nullable();
+            $table->string('dimensions')->nullable();
+            $table->string('overview')->nullable();
+            $table->string('excerpt')->nullable();
+            $table->string('synopsis')->nullable();
+            $table->string('toc')->nullable();
+            $table->string('editorial_reviews')->nullable();
             $table->timestamps();
         });
     }
--- database/seeds/BooksTableSeeder.php
+++ database/seeds/BooksTableSeeder.php
@@ -14,12 +14,12 @@ class BooksTableSeeder extends Seeder
         $now = date('Y-m-d H:i:s');
         
         DB::table('books')->insert([
-            'title'     => 'Book Title',
-            'slug'     => 'book-title',
-            'amazon_id'     => null,
-            'google_id'     => null,
-            'audible_id'     => null,
-            'isbn_13'     => '1234567890123',
+            'title'     => 'In The Plex: How Google Thinks, Works, and Shapes Our Lives',
+            'slug'     => 'in-the-plex',
+            'amazon_id'     => '1416596585',
+            'google_id'     => 'V1u1f8sv3k8C',
+            'audible_id'     => 'B004UN8UD2',
+            'isbn_13'     => '9781416596585',
             'book_cover'     => null,
             'created_by'    => 1,
             'updated_by'    => null,
