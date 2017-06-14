339877d28ae74886330398ea92671e3ceb06f215
DrewRoberts <drew@roberts.email>
2017-06-13 20:48:28 -0400

    Updated Sitemap

--- resources/views/sitemap/index.blade.php
+++ resources/views/sitemap/index.blade.php
@@ -1,20 +1,23 @@
-<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
+<?php 
+echo '<?xml version="1.0" encoding="UTF-8"?>'; 
+$now = date('c',time()); 
+?>
 
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <sitemap>
         <loc>https://tipoff.com/sitemap/main</loc>
-        <lastmod>{{ $quote->updated_at->tz('UTC')->toAtomString() }}</lastmod>
+        <lastmod><?php echo $now; ?></lastmod>
     </sitemap>
     <sitemap>
         <loc>https://tipoff.com/sitemap/quoteauthors</loc>
-        <lastmod>{{ $quoteauthor->updated_at->tz('UTC')->toAtomString() }}</lastmod>
+        <lastmod><?php echo $now; ?></lastmod>
     </sitemap>
     <sitemap>
         <loc>https://tipoff.com/sitemap/quotes</loc>
-        <lastmod>{{ $quote->updated_at->tz('UTC')->toAtomString() }}</lastmod>
+        <lastmod><?php echo $now; ?></lastmod>
     </sitemap>
     <sitemap>
         <loc>https://tipoff.com/sitemap/users</loc>
-        <lastmod>{{ $user->updated_at->tz('UTC')->toAtomString() }}</lastmod>
+        <lastmod><?php echo $now; ?></lastmod>
     </sitemap>
 </sitemapindex>
--- resources/views/sitemap/users.blade.php
+++ resources/views/sitemap/users.blade.php
@@ -2,7 +2,7 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     @foreach ($users as $user)
         <url>
-            <loc>https://tipoff.com/@{{ $user->username }}</loc>
+            <loc>{{ $user->path() }}</loc>
             <lastmod>{{ $user->updated_at->tz('UTC')->toAtomString() }}</lastmod>
             <changefreq>weekly</changefreq>
             <priority>0.7</priority>
