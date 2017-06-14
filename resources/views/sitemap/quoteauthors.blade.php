2b9a6ee0733b3de1dac83b460cf46abcbde9f700
DrewRoberts <drew@roberts.email>
2017-06-13 20:54:28 -0400

    Updated Sitemap

--- resources/views/sitemap/quoteauthors.blade.php
+++ resources/views/sitemap/quoteauthors.blade.php
@@ -2,7 +2,7 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     @foreach ($quoteauthors as $quoteauthor)
         <url>
-            <loc>https://tipoff.com/quotes/{{ $quoteauthor->slug }}</loc>
+            <loc>https://tipoff.com{{ $quoteauthor->path() }}</loc>
             <lastmod>{{ $quoteauthor->updated_at->tz('UTC')->toAtomString() }}</lastmod>
             <changefreq>weekly</changefreq>
             <priority>0.9</priority>
