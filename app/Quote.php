09cdc19e4d11f99796e55f13b0f2c85819feb8b6
DrewRoberts <drew@roberts.email>
2017-06-16 22:46:03 -0400

    Edited quote path

--- app/Quote.php
+++ app/Quote.php
@@ -23,7 +23,7 @@ class Quote extends Model
      */
     public function path()
     {
-        return "/quotes/{$this->author->slug}/{$this->id}";
+        return "{$this->author->path}/{$this->id}";
     }
 
     /**
