4df4b2a8f86c4c70d0a26bfe67c3756e3df448ec
DrewRoberts <drew@roberts.email>
2017-06-22 15:21:49 -0400

    Edited Footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -80,7 +80,7 @@
     <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
 
     <!-- Styles -->
-    <link href="{{ asset('css/stylesc.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/stylesd.css') }}" rel="stylesheet">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -145,47 +145,31 @@
         <footer class="mdl-mega-footer footer-shadow">
           <div class="mdl-mega-footer__middle-section" style="max-width: 970px;margin:auto;padding: 0px 0px 24px 0px;">
 
+            <div class="mdl-mega-footer__drop-down-section first">
+              <input class="mdl-mega-footer__heading-checkbox" type="checkbox" checked>
+              <h3 class="mdl-mega-footer__heading" style="color: #4d4d4d;">Tipoff</h3>
+              <p class="mdl-mega-footer__link-list" style="width: 80%; float: left;">The Tipoff Project is a cooperative effort led by a group of editors, writers, and developers. We provide resources for business and leadership.</p>
+              <div style="width: 20%; float: left;"></div>
+            </div>
+
             <div class="mdl-mega-footer__drop-down-section">
               <input class="mdl-mega-footer__heading-checkbox" type="checkbox" checked>
-              <h1 class="mdl-mega-footer__heading">Quotes</h1>
+              <h3 class="mdl-mega-footer__heading" style="color: #4d4d4d;">Discover</h3>
               <ul class="mdl-mega-footer__link-list">
-                <li><a href="/quotes">Index</a></li>
-                <li><a href="/quotes/authors">Authors</a></li>
-                <li><a href="/quotes/topics">Topics</a></li>
+                <li><a href="/quotes">Quotes</a></li>
+                <li><a href="/books">Books</a></li>
+                <li><a href="/company">About</a></li>
               </ul>
             </div>
 
             <div class="mdl-mega-footer__drop-down-section">
               <input class="mdl-mega-footer__heading-checkbox" type="checkbox" checked>
-              <h1 class="mdl-mega-footer__heading">Books</h1>
-              <ul class="mdl-mega-footer__link-list">
-                <li><a href="/books">Titles</a></li>
-                <li><a href="/books/authors">Authors</a></li>
-                <li><a href="/books/topics">Topics</a></li>
-              </ul>
-            </div>
-
-            <div class="mdl-mega-footer__drop-down-section">
-              <input class="mdl-mega-footer__heading-checkbox" type="checkbox" checked>
-              <h1 class="mdl-mega-footer__heading">About</h1>
-              <ul class="mdl-mega-footer__link-list">
-                <li><a href="/company">Company Intro</a></li>
-                <li><a href="/company/advertising">Advertising</a></li>
-                <li><a href="/company/contact">Contact</a></li>
-                <li><a href="/company/staff">Staff</a></li>
-                <li><a href="/company/team">Team</a></li>
-              </ul>
-            </div>
-
-            <div class="mdl-mega-footer__drop-down-section">
-              <input class="mdl-mega-footer__heading-checkbox" type="checkbox" checked>
-              <h1 class="mdl-mega-footer__heading">Connect</h1>
+              <h3 class="mdl-mega-footer__heading" style="color: #4d4d4d;">Connect</h3>
               <ul class="mdl-mega-footer__link-list" style="width: 100px; float: left;">
                 <li><a href="https://www.facebook.com/Tipoff">Facebook</a></li>
                 <li><a href="https://www.instagram.com/tipoff/">Instagram</a></li>
                 <li><a href="https://www.pinterest.com/tipoff/">Pinterest</a></li>
                 <li><a href="https://twitter.com/TipoffProject">Twitter</a></li>
-
               </ul>
               <ul class="mdl-mega-footer__link-list" style="width: 100px; float: left;">
                 <li><a href="https://www.linkedin.com/company/TipoffProject">LinkedIn</a></li>
