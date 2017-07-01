1ddc6723e05a864982223a8e3e2382c83abd1d65
DrewRoberts <drew@roberts.email>
2017-07-01 16:53:24 -0400

    Edited Registration

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,76 +7,38 @@
 @section('featured_image', 'https://tipoff.com/ogimage.jpg')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('register') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
-
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Register
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
+<div class="mdl-card-square mdl-card mdl-shadow--2dp">
+    <div class="mdl-card__title">
+    <h2 class="mdl-card__title-text">Registration</h2>
+    </div>
+    <div class="mdl-card__supporting-text {{ $errors->has('name') ? ' has-error' : '' }}">
+    <form role="form" method="POST" action="{{ route('register') }}">
+        {{ csrf_field() }}
+      <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+        <input class="mdl-textfield__input" type="text" id="first_name" value="{{ old('first_name') }}" required>
+          <label class="mdl-textfield__label" for="first_name">First Name</label>
+      </div>
+      <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+        <input class="mdl-textfield__input" type="text" id="last_name" value="{{ old('last_name') }}" required>
+          <label class="mdl-textfield__label" for="last_name">Last Name</label>
+      </div>
+      <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+        <input class="mdl-textfield__input" id="email" type="email" name="email" value="{{ old('email') }}" required>
+          <label class="mdl-textfield__label" for="email">Email</label>
+      </div>
+      <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+        <input class="mdl-textfield__input" id="password" type="password" name="password" required>
+          <label class="mdl-textfield__label" for="password">Password</label>
+      </div>
+      <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+        <input class="mdl-textfield__input" id="password-confirm" type="password" name="password_confirmation" required>
+          <label class="mdl-textfield__label" for="password-confirm">Retype   Password</label>
+      </div>
+      <div class="mdl-card__actions mdl-card--border">
+      <button type="submit" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
+      Register
+      </button>
+    </form>
     </div>
 </div>
 @endsection
