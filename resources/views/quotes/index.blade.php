4f3410888d66e9f59e05804a870762b6e04de8a6
DrewRoberts <drew@roberts.email>
2017-07-03 11:56:21 -0400

    Edited Sidebar

--- resources/views/quotes/index.blade.php
+++ resources/views/quotes/index.blade.php
@@ -35,6 +35,24 @@
             @endforeach
         </div>
         <div class="sidebar mdl-cell mdl-cell--4-col-phone">
+            <paper-card image="/ogimage.jpg">
+              <div class="card-content">
+                <div class="sidebar-connect">Connect with Tipoff</div>
+                <p class="sidebar-connect-p">Receive daily quotes and more resources for business and leadership by connecting with us on social media.</p>
+              </div>
+              <div class="card-actions">
+                <div class="horizontal justified">
+                    <a href="https://www.facebook.com/Tipoff" tabindex="-1"><paper-icon-button class="social" src="/img/facebook.png"></paper-icon-button></a>
+                    <a href="https://www.instagram.com/tipoff/" tabindex="-1"><paper-icon-button class="social" style="color: red;" src="/img/instagram.png"></paper-icon-button></a>
+                    <a href="https://www.pinterest.com/tipoff/" tabindex="-1"><paper-icon-button class="social" style="color: red;" src="/img/pinterest.png"></paper-icon-button></a>
+                    <a href="https://twitter.com/TipoffProject" tabindex="-1"><paper-icon-button src="/img/twitter.png"></paper-icon-button></a>
+                    <a href="https://plus.google.com/+Tipoff" tabindex="-1"><paper-icon-button src="/img/google-plus.png"></paper-icon-button></a>
+                    <a href="https://www.linkedin.com/company/TipoffProject" tabindex="-1"><paper-icon-button src="/img/linkedin.png"></paper-icon-button></a>
+                </div>
+              </div>
+            </paper-card>
+            <paper-card>
+            </paper-card>
             @foreach ($sidequotes as $quote)
                 <tipoff-quote id="{{ $quote->id }}" quote="{{ $quote->quote_text }}" author="{{ $quote->author->display_name }}" slug="{{ $quote->author->slug }}" more="70" @if (in_array($quote->author->id, $avatars )) avatar="/img/{{ $quote->author->slug }}.jpg" @endif @if ($loop->first) graphics @endif ></tipoff-quote>
                 <br>
