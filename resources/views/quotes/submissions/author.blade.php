7040b78a3620fcb1baf4f81e32a6dd181630d68f
DrewRoberts <drew@roberts.email>
2017-07-20 22:33:46 -0400

    Updated to HTTPS Google Font Links

--- resources/views/quotes/submissions/author.blade.php
+++ resources/views/quotes/submissions/author.blade.php
@@ -46,7 +46,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.grey-red.min.css">
     <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,100,500,300italic,500italic,700italic,900,300">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,100,500,300italic,500italic,700italic,900,300">
 
     <!-- Styles -->
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
