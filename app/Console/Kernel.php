0a88436284ad526b2930f713f4c88fe4a75365d6
DrewRoberts <drew@roberts.email>
2017-08-02 14:31:48 -0400

    Edited Scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {   
-        //$schedule->command('startergraphics')->withoutOverlapping();
+        $schedule->command('startergraphics')->withoutOverlapping();
         //$schedule->command('startergraphics')->everyThirtyMinutes();
     }
 
