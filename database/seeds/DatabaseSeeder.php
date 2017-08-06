cec558454f6dcddb4da5af27d8ee5d0cdf46ecee
DrewRoberts <drew@roberts.email>
2017-08-06 13:52:53 -0400

    Update DB Seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Run the database seeds. -DR
      *
      * @return void
      */
