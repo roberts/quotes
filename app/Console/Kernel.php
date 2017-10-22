a1eed65a57ccc2add1a119cebef899bef065a4f0
DrewRoberts <drew@roberts.email>
2017-10-21 22:24:38 -0400

    Create Articles Model

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {   
         //$schedule->command('startergraphics')->withoutOverlapping();
-        $schedule->command('startergraphics')->everyThirtyMinutes();
+        //$schedule->command('startergraphics')->everyThirtyMinutes();
     }
 
     /**
---
+++ database/migrations/2017_10_22_022339_create_articles_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) { // Should we use this for the oral history posts too?
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
